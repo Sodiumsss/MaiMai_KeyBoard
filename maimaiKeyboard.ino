593fed9dde97cd22223cf5be8c83d835749fa550
Sodiums <sodiums@vip.qq.com>
2024-01-06 14:55:14 +0800

    Add files via upload
    写错了

--- maimaiKeyboard.ino
+++ maimaiKeyboard.ino
@@ -8,12 +8,12 @@
 #define TOTAL_BUTTON_COUNT 8
 
 //按键检测间隔，单位：毫秒
-#define BOUNCE_INTERVAL 10
+#define BOUNCE_INTERVAL 5
 
 //引脚
-int buttonPin[] = {4,2,20,18,19,21,5,3,4};
+int buttonPin[] = {4,2,20,18,19,21,5,3};
 //引脚对应的按键
-char buttonKey[] = {'q','w','e','d','c','x','z','a','s'};
+char buttonKey[] = {'q','w','e','d','c','x','z','a'};
 
 Bounce buttonBounce[TOTAL_BUTTON_COUNT];
 bool buttonState[TOTAL_BUTTON_COUNT];
@@ -35,16 +35,19 @@ void loop() {
 
   for (int i = 0; i < TOTAL_BUTTON_COUNT; i++){
    buttonBounce[i].update();
+
+   //低电平按下
    buttonState[i] = (buttonBounce[i].read() == LOW);
+   
    if (buttonState[i] && !previousState[i]) { 
     Keyboard.press(buttonKey[i]);
-    Serial.print("[+]");
-    Serial.println(buttonKey[i]);
+//    Serial.print("[+]");
+//    Serial.println(buttonKey[i]);
 
    } else if (!buttonState[i] && previousState[i]) {
     Keyboard.release(buttonKey[i]);
-    Serial.print("[-]");
-    Serial.println(buttonKey[i]);
+//    Serial.print("[-]");
+//    Serial.println(buttonKey[i]);
    }
    previousState[i] = buttonState[i];
   }
